b07c6c7ee7639ed7300da7f878145fa772ed4d51
Haru <gabs.haru@gmail.com>
2024-06-13 11:31:29 -0400

    Create Batalha main class

---
+++ TrabalhoRPG/src/rpgzinho/Batalha.java
@@ -0,0 +1,16 @@
+package rpgzinho;
+import java.util.Scanner;
+
+public class Batalha {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String personagemA, personagemB;
+		
+		personagemA = scanner.next();
+		personagemB = scanner.next();
+
+		
+	}
+
+}
