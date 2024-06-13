55e835a72fe7728773dd7c7eb67df4f93316d664
Haru <gabs.haru@gmail.com>
2024-06-13 10:52:39 -0400

    Moved implementation to superclass

--- TrabalhoRPG/src/rpgzinho/Clerigo.java
+++ TrabalhoRPG/src/rpgzinho/Clerigo.java
@@ -2,38 +2,10 @@ package rpgzinho;
 
 public class Clerigo extends Personagem{
 
-	public Clerigo(String nomeTipo, double saude, double forca, double destreza, Arma arma) {
+	public Clerigo(String nomeTipo, double saude, double forca, double destreza, ArmaClerigo arma) {
 		super(nomeTipo, saude, forca, destreza, arma);
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	public void printStatus() {
-		System.out.printf("Clérigo [Saude: %.f, Forca: %.f, DEstreza: %.f, " + arma.getNome());
-	}
-
-	@Override
-	public void atacar(Personagem alvo) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public double calculoDano() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void recebeDano() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean estaMorto() {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
 }
